239d0a76b6135ba9715caa6da3a28a04ce39989f
NadishiChristina <nadishiramanayake@gmail.com>
2025-03-02 15:34:19 +0530

    initial commit

---
+++ src/main/java/com/sdgp/achieveplusapi/AchievePlusApiApplication.java
@@ -0,0 +1,13 @@
+package com.sdgp.achieveplusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievePlusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchievePlusApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdgp/achieveplusapi/AchievePlusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdgp.achieveplusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchievePlusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
