733dab314a87aa5db56c9bb77fe50e731618ea0e
NadishiChristina <nadishiramanayake@gmail.com>
2025-03-02 17:05:15 +0530

    Added the chart data

---
+++ src/main/java/com/sdgp/achieveplusapi/model/ChartData.java
@@ -0,0 +1,67 @@
+package com.sdgp.achieveplusapi.model;
+
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class ChartData {
+    private List<String> labels;
+    private List<Dataset> datasets;
+
+    @Data
+    public static class Dataset {
+        private String label;
+        private List<Integer> data;
+        private String borderColor;
+        private String backgroundColor;
+
+        public String getLabel() {
+            return label;
+        }
+
+        public void setLabel(String label) {
+            this.label = label;
+        }
+
+        public List<Integer> getData() {
+            return data;
+        }
+
+        public void setData(List<Integer> data) {
+            this.data = data;
+        }
+
+        public String getBorderColor() {
+            return borderColor;
+        }
+
+        public void setBorderColor(String borderColor) {
+            this.borderColor = borderColor;
+        }
+
+        public String getBackgroundColor() {
+            return backgroundColor;
+        }
+
+        public void setBackgroundColor(String backgroundColor) {
+            this.backgroundColor = backgroundColor;
+        }
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<String> labels) {
+        this.labels = labels;
+    }
+
+    public List<Dataset> getDatasets() {
+        return datasets;
+    }
+
+    public void setDatasets(List<Dataset> datasets) {
+        this.datasets = datasets;
+    }
+}
